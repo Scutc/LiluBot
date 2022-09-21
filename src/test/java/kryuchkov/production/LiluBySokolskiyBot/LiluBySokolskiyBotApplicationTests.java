b39fd79672f533bdd923c814a3d00ee24bd23b3f
DmitryKryuchkov <dmitrykryuchkov@MacBook-Pro-Dmitry.local>
2022-09-21 22:34:48 +0300

    Создан пустой проект Spring

---
+++ src/test/java/kryuchkov/production/LiluBySokolskiyBot/LiluBySokolskiyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kryuchkov.production.LiluBySokolskiyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiluBySokolskiyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
